19a34fb3b7022534ee40298d8d0224744ef8b4b2
David Chou <zerohated@hotmail.com>
2018-05-02 23:12:56 +0800

    update 349.intersection-of-two-arrays.go

---
+++ 349.intersection-of-two-arrays.go
@@ -0,0 +1,51 @@
+/*
+ * [349] Intersection of Two Arrays
+ *
+ * https://leetcode.com/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (48.30%)
+ * Total Accepted:    131.1K
+ * Total Submissions: 271.4K
+ * Testcase Example:  '[]\n[]'
+ *
+ *
+ * Given two arrays, write a function to compute their intersection.
+ *
+ *
+ * Example:
+ * Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+ *
+ *
+ * Note:
+ *
+ * Each element in the result must be unique.
+ * The result can be in any order.
+ *
+ *
+ */
+
+func intersection(nums1 []int, nums2 []int) []int {
+	result := []int{}
+	mapN1, mapN2 := make(map[int]bool), make(map[int]bool)
+	for _, item := range nums1 {
+		mapN1[item] = true
+	}
+	for _, item := range nums2 {
+		mapN2[item] = true
+	}
+	if len(mapN1) < len(mapN2) {
+		for num := range mapN1 {
+			if mapN2[num] == true {
+				result = append(result, num)
+			}
+		}
+	} else {
+		for num := range mapN2 {
+			if mapN1[num] == true {
+				result = append(result, num)
+			}
+		}
+	}
+	return result
+}
